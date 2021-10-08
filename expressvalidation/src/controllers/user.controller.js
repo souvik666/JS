cd80d798cde3fe0d21b7614a6324c5fd1eb23c2c
souvik666 <peacedynasty0@gmail.com>
2021-10-08 21:05:57 +0530

    Express Validations

--- expressvalidation/src/controllers/user.controller.js
+++ expressvalidation/src/controllers/user.controller.js
@@ -1,8 +1,22 @@
+/* and try to implement below validations on these inputs
+first_name => required
+last_name => required
+email => required and should be a valid email
+pincode => required and should be exactly 6 numbers
+age => required and should be between 1 and 100.
+gender => required and should be either Male, Female or Others */
+
 const express = require("express");
 const router = express.Router();
 const user = require("../models/user.model");
 const { body, validationResult } = require("express-validator");
 
+router.get("/", async (req, res) => {
+  const myres = await user.find().lean().exec();
+  res.send({ users: myres });
+});
+
+/* create a user */
 router.post(
   "/",
   body("last_name").isLength({ min: 3 }).withMessage("please enter an name"),
